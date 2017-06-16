9c548f429bce94d2ed0507499477161ea29eee78
Nguyễn Đặng Hoài Duy <ngray1747@gmail.com>
2017-06-16 13:25:11 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Program Files (x86)/Ampps/www/catalog/');
+define('DIR_SYSTEM', 'C:/Program Files (x86)/Ampps/www/system/');
+define('DIR_IMAGE', 'C:/Program Files (x86)/Ampps/www/image/');
+define('DIR_LANGUAGE', 'C:/Program Files (x86)/Ampps/www/catalog/language/');
+define('DIR_TEMPLATE', 'C:/Program Files (x86)/Ampps/www/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/Program Files (x86)/Ampps/www/system/config/');
+define('DIR_CACHE', 'C:/Program Files (x86)/Ampps/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/Program Files (x86)/Ampps/www/system/storage/download/');
+define('DIR_LOGS', 'C:/Program Files (x86)/Ampps/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/Program Files (x86)/Ampps/www/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/Program Files (x86)/Ampps/www/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'mysql');
+define('DB_DATABASE', 'opencart_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
