03d0a14fa04a9e7ccdcc83934e6591a3d6ed6bcb
ngray1747 <ngray1747@gmail.com>
2017-07-04 23:27:51 +0700

    Update catalog/language/vi-vn/module/multimerch_comments.php

--- catalog/language/vi-vn/module/multimerch_comments.php
+++ catalog/language/vi-vn/module/multimerch_comments.php
@@ -4,26 +4,26 @@
 $_['ms_account_dashboard_comments'] = 'Last comments';
 
 // Account - Comments
-$_['ms_account_comments_name'] = 'Name';
-$_['ms_account_comments_product'] = 'Product';
-$_['ms_account_comments_comment'] = 'Comment';
-$_['ms_account_comments_nocomments'] = 'You don\'t have any comments yet!';
+$_['ms_account_comments_name'] = 'Tên';
+$_['ms_account_comments_product'] = 'Sản phẩm';
+$_['ms_account_comments_comment'] = 'Bình luận';
+$_['ms_account_comments_nocomments'] = 'Bạn chưa có bình luận nào!';
 
 // Product page - Comments
-$_['ms_comments_post_comment'] = 'Post Comment';
-$_['ms_comments_name'] = 'Name';
+$_['ms_comments_post_comment'] = 'Đăng bình luận';
+$_['ms_comments_name'] = 'Tên';
 $_['ms_comments_note'] = '<span style="color: #FF0000;">Note:</span> HTML is not translated!';
 $_['ms_comments_email'] = 'E-mail';
-$_['ms_comments_comment'] = 'Comment';
-$_['ms_comments_wait'] = 'Please Wait!';
+$_['ms_comments_comment'] = 'Bình luận';
+$_['ms_comments_wait'] = 'Vui lòng chờ!';
 $_['ms_comments_login_register'] = 'Please <a href="%s">login</a> or <a href="%s">register</a> to post a comment.';
-$_['ms_comments_error_name'] = 'Please enter a name between %s and %s characters long';
-$_['ms_comments_error_email'] = 'Please enter a valid email';
-$_['ms_comments_error_comment_short'] = 'The comment body must be at least %s characters long';
-$_['ms_comments_error_comment_long'] = 'The comment body cannot be longer than %s characters';
-$_['ms_comments_error_captcha'] = 'Verification code does not match the image';
-$_['ms_comments_success'] = 'Thank you for your comment.';
-$_['ms_comments_captcha'] = 'Enter the code in the box below:';
+$_['ms_comments_error_name'] = 'Tên phải từ %s đến %s ký tự';
+$_['ms_comments_error_email'] = 'Vui lòng nhập lại email';
+$_['ms_comments_error_comment_short'] = 'Comment phải nhiểu hơn %s ký tự';
+$_['ms_comments_error_comment_long'] = 'Comment không thể nhiều hơn %s ký tự';
+$_['ms_comments_error_captcha'] = 'Mã không đúng';
+$_['ms_comments_success'] = 'Cảm ơn bình luận của bạn.';
+$_['ms_comments_captcha'] = 'Vui lòng nhập mã bên dưới:';
 $_['ms_comments_no_comments_yet'] = 'No comments added yet';
 $_['ms_comments_tab_comments'] = 'Comments (%s)';
 
